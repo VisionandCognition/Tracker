99904d48f1a4079a27e1bb661c8cd6dae5385863
chrisvdt <cvandert@gmail.com>
2016-02-24 14:02:22 +0100

    Readbit added to DIO24 library

--- DIO24/IOdas.h
+++ DIO24/IOdas.h
@@ -9,6 +9,7 @@ extern "C"	DIO24_API bool InitIO( WORD BoardNum);
 
 extern "C"	DIO24_API void WritePortA(WORD val);
 extern "C"	DIO24_API void Writebit(WORD bitn, WORD val);
+extern "C"	DIO24_API WORD Readbit(WORD bitn);
 
 
 
--- DIO24/dascompile64.m
+++ DIO24/dascompile64.m
@@ -4,7 +4,9 @@
 
 
 
-mex('-LI:\Users\togt\Documents\matlab\mex\DIO24', '-lPCIeDIO24', 'dasinit.cpp' )
+mex('-LI:\Users\togt\Documents\GitHub\VCscripts\DIO24', '-lPCIeDIO24', 'dasinit.cpp' )
 
-mex('-LI:\Users\togt\Documents\matlab\mex\DIO24', '-lPCIeDIO24', 'dasbit.cpp' )
+mex('-LI:\Users\togt\Documents\GitHub\VCscripts\DIO24', '-lPCIeDIO24', 'dasbit.cpp' )
+
+mex('-LI:\Users\togt\Documents\GitHub\VCscripts\DIO24', '-lPCIeDIO24', 'dasreadbit.cpp' )
 
