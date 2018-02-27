3a4810e88b688765bed5caec8fb6bba99ba9f39b
chrisvdt <cvandert@gmail.com>
2018-02-27 12:28:31 +0100

    JSON removed

--- JSON/DbVCparms.m
+++
@@ -1,7 +0,0 @@
-function vcpar = DbVCparms()
-%database parameters
-    vcpar.User = 'dbuser_vc2nin'; 
-    vcpar.Passw = 'Y3gr3Dc8ZmsvUuCJ'; 
-    vcpar.Database = 'V&C';
-    vcpar.Server = '192.87.10.117:3307';
-
