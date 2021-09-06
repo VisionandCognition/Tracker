98dd954d7dd1e155536bc3135ae6de81be73287e
Paolo Papale <PPthe2nd@users.noreply.github.com>
2021-09-06 14:52:00 +0200

    Added x and y directions

--- TrackerT/updateTransform.m
+++ TrackerT/updateTransform.m
@@ -5,8 +5,8 @@ Par.ROTx = sqrt(2)/30*Par.SCx;
 Par.ROTy = -sqrt(2)/60*Par.SCy;
 
 T = Par.T;
-T(1,1) = Par.SCx;
-T(2,2) = Par.SCy;
+T(1,1) = Par.SCx * Par.xdir;
+T(2,2) = Par.SCy * Par.ydir;
 % T(3,1) = Par.OFFx;
 % T(3,2) = Par.OFFy;
 T(1,2) = Par.ROTx;
